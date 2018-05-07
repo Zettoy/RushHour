5bf3ff5067933c591fd0f3b64b67cfdfb0ab91c2
aoesora <aoesora@outlook.com>
2018-05-07 21:34:52 +1000

    Mock up user interface

--- Car/src/Game.java
+++ Car/src/Game.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class Game implements GameInterface {	
 	private MapInterface map;
@@ -10,14 +9,9 @@ public class Game implements GameInterface {
 	
 	public void gameStart() {
 		generateMap();
-		//consolePlay();
 	}
 	
-	public void generateMap() {
-		mapGenerator.createMap();
-		map = mapGenerator.getMap();
-	}
-	
+	@Override
 	public boolean isWin() {
 		if (map.getCar(Constants.RED).getPosition().getX() == 4) return true;
 		
@@ -29,41 +23,8 @@ public class Game implements GameInterface {
 		return map;
 	}
 	
-	private void consolePlay() {
-		System.out.println("Type your command at console.");
-		System.out.println("Example:\"2 D\" move car 2 down once");
-		System.out.println();
-		map.print();
-		System.out.print("Command: ");
-		
-		Scanner sc = new Scanner(System.in);
-		while (sc.hasNextLine()) {
-			String[] command = sc.nextLine().split(" ");
-			int carId = Integer.parseInt(command[0]);
-			int direction = 0;
-			switch(command[1]) {
-				case "L":
-					direction = Constants.LEFT;
-					break;
-				case "R":
-					direction = Constants.RIGHT;
-					break;
-				case "U":
-					direction = Constants.UP;
-					break;
-				case "D":
-					direction = Constants.DOWN;
-					break;
-			}
-			map.moveCar(carId, direction);
-			System.out.println();
-			map.print();
-			if (isWin()) {
-				System.out.println("Win");
-				break;
-			}
-			System.out.print("Command: ");
-		}
-		sc.close();
+	private void generateMap() {
+		mapGenerator.createMap();
+		map = mapGenerator.getMap();
 	}
 }
