e0ef82f9392bdd2613c445eb82390c33577e9d1c
jminett <30460521+jminett@users.noreply.github.com>
2018-05-09 15:51:47 +1000

    Added finish screen

--- Car/src/KeyInput.java
+++ Car/src/KeyInput.java
@@ -11,36 +11,32 @@ public class KeyInput implements KeyListener {
 	}
 	
 	@Override
-	public void keyPressed(KeyEvent e) {		
-		if (e.getKeyCode() == KeyEvent.VK_UP) {
-			game.moveCar(Constants.UP);
-			
-		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			game.moveCar(Constants.DOWN);
-			
-		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-			game.moveCar(Constants.LEFT);
-			
-		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			game.moveCar(Constants.RIGHT);
-			
-		} else if (e.getKeyCode() >= KeyEvent.VK_A &&
-				   e.getKeyCode() <= KeyEvent.VK_Z) {
-			int carId = e.getKeyCode() - KeyEvent.VK_A + 1;
-			game.selectCar(carId);
-		
-		} else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
 			game.gameRestart();
-			
-		} else {
-			throw new IllegalStateException();
-		}
+		} else if (!game.isWin())
+			if (e.getKeyCode() == KeyEvent.VK_UP) {
+				game.moveCar(Constants.UP);
+
+			} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+				game.moveCar(Constants.DOWN);
+
+			} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				game.moveCar(Constants.LEFT);
+
+			} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				game.moveCar(Constants.RIGHT);
+
+			} else if (e.getKeyCode() >= KeyEvent.VK_A &&
+					   e.getKeyCode() <= KeyEvent.VK_Z) {
+				int carId = e.getKeyCode() - KeyEvent.VK_A + 1;
+				game.selectCar(carId);
+
+			} else {
+				throw new IllegalStateException();
+			}
 		
 		gamePanel.repaint();
-		
-		if (game.isWin()) {
-			gamePanel.setFocusable(false);
-		}
 	}
 
 	@Override
