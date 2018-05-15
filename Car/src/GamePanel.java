78fa6c5215a74dd36e47e454b80b7a955ea6ed29
aoesora <aoesora@outlook.com>
2018-05-15 12:33:33 +1000

    Drag & Drop

--- Car/src/GamePanel.java
+++ Car/src/GamePanel.java
@@ -2,21 +2,41 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.KeyEvent;
+import java.awt.Point;
+import java.util.ArrayList;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class GamePanel extends JPanel {
 	private GameInterface game;
+	private ArrayList<CarComponent> cars;
+	
+	private Point mousePoint;
 	
 	public GamePanel (GameInterface game) {
 		this.game = game;
+		this.cars = new ArrayList<>();
 		
 		this.setLayout(null);
 		this.setBackground(Color.WHITE);
 		this.addKeyListener(new KeyInput(game, this));
+		this.addMouseListener(new MousePress(game, this));
+		this.addMouseMotionListener(new MouseDragDrop(game, this));
 		this.setFocusable(true);
 		this.requestFocus();
+
+	}
+	
+	public ArrayList<CarComponent> getCars() {
+		return cars;
+	}
+	
+	public void setMousePoint(Point mousePoint) {
+		this.mousePoint = mousePoint;
+	}
+	
+	public Point getMousePoint() {
+		return mousePoint;
 	}
 	
 	@Override
@@ -29,48 +49,60 @@ public class GamePanel extends JPanel {
 		Graphics2D g2d = (Graphics2D) g.create();
 		
 		g2d.setColor(Color.GRAY);
-		for (int i = 0; i < 6; i ++) {
-			for (int j = 0; j < 6; j ++) {
+		for (int i = 0; i < 6; i ++)
+			for (int j = 0; j < 6; j ++)
 				g2d.drawRect(i * 70 + 90, j * 70 + 80, 60, 60);
-			}
-		}
 
 		g.setFont(new Font("Arial", Font.PLAIN,20));
 		g.setColor(Color.BLACK);
-		g.drawString("Moves Made: "+ game.getMovesMade(),90 , 70);
+		g.drawString("Moves Made: "+ game.getMovesMade(), 90 , 70);
 
 		int numCars = game.getMap().getNumCars();
 		g.setFont(new Font("Arial", Font.BOLD, 25));
 		g.setColor(Color.WHITE);
+		
 		for (int i = 0; i < numCars; i ++) {
-			CarInterface car = game.getMap().getCar(i + Constants.RED);
-			Position p = car.getPosition();
+			if (cars.size() >= numCars) break;
+			
+			int carId = i + Constants.RED;
+			cars.add(new CarComponent(carId));
+		}
+		
+		for (CarComponent c : cars) {
+			CarInterface car = game.getMap().getCar(c.getCarId());
 			Character direction = car.getDirection();
 			int length = car.getLength();
 			
+			Position p = car.getPosition();
+			int x = p.getX() * 70 + 90;
+			int y = p.getY() * 70 + 80;
+			c.setStartX(x);
+			c.setStartY(y);
+			
 			if (car.isRedCar()) {
 				g2d.setColor(Color.RED);
-				g2d.fillRect(p.getX() * 70 + 90 , p.getY() * 70 + 80, 132, 62);
+				g2d.fillRect(x , y, 132, 62);
+				c.setEndX(x + 132);
+				c.setEndY(y + 62);
+				
 			} else {
 				g2d.setColor(Color.BLUE);
-				
-				int x = p.getX() * 70 + 90;
-				int y = p.getY() * 70 + 80;
 				
 				if (direction == Constants.HORIZONTAL) {
 					int width = 60 * length + 10 * (length - 1) + 2;
 					int height = 62;
 					g2d.fillRect(x, y, width, height);
+					c.setEndX(x + width);
+					c.setEndY(y + height);
+					
 				} else if (direction == Constants.VERTICAL) {
 					int width = 62;
 					int height = 60 * length + 10 * (length - 1) + 2;
 					g2d.fillRect(x, y, width, height);
+					c.setEndX(x + width);
+					c.setEndY(y + height);
 				}
 			}
-
-			Character carIdForDisplay = (char) (i + KeyEvent.VK_A);
-
-			g.drawString(carIdForDisplay + "", p.getX() * 70 + 95 , p.getY() * 70 + 105);
 			
 		}
 
@@ -88,9 +120,11 @@ public class GamePanel extends JPanel {
 		}
 		
 		CarInterface selectedCar = game.getMap().getCar(game.getSelectedCar());
+		if (selectedCar == null) return;
 		Position selectedP = selectedCar.getPosition();
 		
 		g2d.setColor(Color.WHITE);
 		g2d.fillRect(selectedP.getX() * 70 + 95, selectedP.getY() * 70 + 85, 20, 20);
+		
 	}
 }
